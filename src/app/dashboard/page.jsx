c1960a717bc1b2704b7d3fa09d431147905135b5
Nou7en <tomh.tc89@gmail.com>
2024-10-08 01:44:45 -0500

    ajustes

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -5,7 +5,7 @@ function DashboardPage() {
   return (
     <section className="h-[calc(100vh-7rem)] flex justify-center items-center">
       <div>
-        <h1 className="text-white text-5xl">Dashboard</h1>
+        <h1 className="text-white text-5xl">Pagina Privada</h1>
         <button className="bg-white text-black px-4 py-2 rounded-md mt-4"
           onClick={() => signOut()}
         >
